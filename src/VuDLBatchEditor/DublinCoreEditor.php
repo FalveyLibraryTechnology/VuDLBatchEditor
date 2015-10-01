054311aa02e9e0646468fc6027224838cf99569a
Demian Katz <demian.katz@villanova.edu>
2015-10-01 16:01:55 -0400

    Tabs to spaces.

--- src/VuDLBatchEditor/DublinCoreEditor.php
+++ src/VuDLBatchEditor/DublinCoreEditor.php
@@ -3,21 +3,21 @@ namespace VuDLBatchEditor;
 
 class DublinCoreEditor
 {
-	protected $ns = 'http://purl.org/dc/elements/1.1/';
-	protected $xml;
+    protected $ns = 'http://purl.org/dc/elements/1.1/';
+    protected $xml;
 
-	public function __construct($xml)
-	{
-		$this->xml = simplexml_load_string($xml);
-	}
+    public function __construct($xml)
+    {
+        $this->xml = simplexml_load_string($xml);
+    }
 
-	public function addChild($field, $value)
-	{
-		$this->xml->addChild($field, $value, $this->ns);
-	}
+    public function addChild($field, $value)
+    {
+        $this->xml->addChild($field, $value, $this->ns);
+    }
 
     public function getXml()
-	{
-		return $this->xml->asXml();
-	}
+    {
+        return $this->xml->asXml();
+    }
 }
